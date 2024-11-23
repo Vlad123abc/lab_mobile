9721522e8a83f278200785172a125ef650c895ec
Vlad123abc <vladi.ivanescu@gmail.com>
2024-11-23 12:48:17 +0200

    debug client

--- src/pages/CarList.tsx
+++ src/pages/CarList.tsx
@@ -1,40 +1,53 @@
-import React, {useContext, useEffect, useState} from "react";
-import Car from "./Car";
-import {IonButton, IonInfiniteScroll, IonInfiniteScrollContent, IonInput} from "@ionic/react";
+import React, { useContext, useEffect, useState } from "react";
+import { IonButton, IonInput, IonInfiniteScroll, IonInfiniteScrollContent } from "@ionic/react";
 import { useHistory } from "react-router";
 import { CarsContext } from "../App";
 import { WsStateContext } from "../App";
+import Car from "./Car";
 import NetworkStatus from "../components/NetworkStatus";
 import axios from "axios";
 import {CarProps} from "./CarProps";
 
 const CarList = (): React.JSX.Element => {
   const history = useHistory();
-  const { cars } = useContext(CarsContext);
-  const { wsState, setWsState } = useContext(WsStateContext);
+  const { cars, setCars } = useContext(CarsContext);
+  const { wsState } = useContext(WsStateContext);
+  const [filter, setFilter] = useState(""); // State to track the filter input
+  const [offset, setOffset] = useState(0); // Offset for pagination
+  const [loading, setLoading] = useState(false); // Loading state for infinite scroll
 
-    const [filter, setFilter] = useState(""); // State to track the filter input
+  useEffect(() => {
+    loadMoreCars();
+  }, []);
 
-    // Filter the car list based on the input
-    const filteredCars = cars.filter((car) =>
-        car.brand.toLowerCase().includes(filter.toLowerCase())
-    );
+  const loadMoreCars = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get<CarProps[]>(
+          `http://localhost:3000/car?offset=${offset}&limit=3`, // Fetch cars in batches of 3
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+      );
+      setCars((prevCars) => [...prevCars, ...response.data]);
+      setOffset((prevOffset) => prevOffset + response.data.length);
+    } catch (error) {
+      console.log("Failed to load more cars:", error);
+    }
+    setLoading(false);
+  };
+
+  // Filter the car list based on the input
+  const filteredCars = cars.filter((car) =>
+      car.brand.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
-    <div>
       <div>
-        <NetworkStatus />
-
-        {/*{cars.map((car) => (*/}
-        {/*  <Car*/}
-        {/*    key={car.id}*/}
-        {/*    id={car.id}*/}
-        {/*    brand={car.brand}*/}
-        {/*    date={car.date}*/}
-        {/*    is_new={car.is_new}*/}
-        {/*  />*/}
-        {/*))}*/}
-
+        <div>
+          <NetworkStatus />
           {/* Display filtered cars */}
           {filteredCars.map((car) => (
               <Car
@@ -48,22 +61,34 @@ const CarList = (): React.JSX.Element => {
 
           {/* Input field for filtering */}
           <IonInput
-              placeholder={"Filter by brand"}
+              placeholder="Filter by brand"
               value={filter}
               onIonChange={(e) => setFilter(e.detail.value || "")}
-              //clearInput
           />
 
-        <IonButton
-          onClick={(e) => {
-            e.preventDefault();
-            history.push("/carsadd");
-          }}
-        >
-          Add cars
-        </IonButton>
+          <IonButton
+              onClick={(e) => {
+                e.preventDefault();
+                history.push("/carsadd");
+              }}
+          >
+            Add cars
+          </IonButton>
+
+          {/* Infinite scroll component */}
+          <IonInfiniteScroll
+              threshold="100px"
+              onIonInfinite={(e) => {
+                loadMoreCars().then(() => e.target.complete());
+              }}
+              disabled={loading}
+          >
+            <IonInfiniteScrollContent
+                loadingText="Loading more cars..."
+            ></IonInfiniteScrollContent>
+          </IonInfiniteScroll>
+        </div>
       </div>
-    </div>
   );
 };
 
