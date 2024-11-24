0f31b195b6a6a65cb956c1d7332f29fd5b00ec7c
pifto <pifto@hell.org>
2024-11-24 07:15:58 +0200

    Work in progress: images

--- src/pages/AddCarPage.tsx
+++ src/pages/AddCarPage.tsx
@@ -1,11 +1,18 @@
 import React, { useContext, useEffect } from "react";
-import { IonButton, IonCheckbox, IonDatetime, IonInput } from "@ionic/react";
+import {
+  IonButton,
+  IonCheckbox,
+  IonDatetime,
+  IonInput,
+  IonLabel,
+} from "@ionic/react";
 import { CarAction, CarProps } from "./CarProps";
 import axios from "axios";
 import { useHistory } from "react-router";
 import { AuthContext } from "../App";
 import { ActionsContext } from "../App";
 import { processActionQueue } from "../App";
+
 const AddCarPage = (): React.JSX.Element => {
   const { token } = useContext(AuthContext);
   const { actions: actions, setActions: setActions } =
@@ -18,6 +25,24 @@ const AddCarPage = (): React.JSX.Element => {
     car_image: "",
   });
   const history = useHistory();
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("handle file change!");
+    const file = e.target.files?.[0];
+
+    console.log("file is:", file);
+    if (file) {
+      console.log("going to read the:", file);
+
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        console.log("the image blob we read:", reader.result as string);
+        setCar((prevState) => {
+          return { ...prevState, car_image: reader.result as string };
+        });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
   return (
     <div>
@@ -48,6 +73,23 @@ const AddCarPage = (): React.JSX.Element => {
       >
         Is new
       </IonCheckbox>
+      <div>
+        {" "}
+        {car.car_image && (
+          <div style={{ marginTop: "20px" }}>
+            <img
+              src={car.car_image}
+              alt="Car Preview"
+              style={{ width: "100%", maxWidth: "300px", height: "auto" }}
+            />
+          </div>
+        )}
+      </div>
+      <div>
+        <IonLabel position="stacked">Image2</IonLabel>
+        <input type="file" accept="image/*" onChange={handleFileChange} />
+      </div>
+
       <div>
         <IonButton
           onClick={() => {
--- src/pages/CarList.tsx
+++ src/pages/CarList.tsx
@@ -9,6 +9,7 @@ import {
   IonRow,
   IonCol,
   IonInput,
+  IonContent,
 } from "@ionic/react";
 import { useHistory } from "react-router";
 import { CarsContext } from "../App";
@@ -42,81 +43,83 @@ const CarList = (): React.JSX.Element => {
   }
 
   return (
-    <div>
+    <IonContent>
       <div>
-        <NetworkStatus />
-        {/* Display filtered cars */}
-        <IonGrid className="car-table">
-          <IonRow className="header-row">
-            <IonCol>
-              <strong>ID</strong>
-            </IonCol>
-            <IonCol>
-              <strong>Brand</strong>
-            </IonCol>
-            <IonCol>
-              <strong>Date</strong>
-            </IonCol>
-            <IonCol>
-              <strong>Is new?</strong>
-            </IonCol>
-            <IonCol>
-              <strong>Picture</strong>
-            </IonCol>
-            <IonCol>
-              <strong>Action</strong>
-            </IonCol>
-          </IonRow>
-          {cars.map((car, index) => (
-            <IonRow
-              key={car.id}
-              className={index % 2 === 0 ? "even-row" : "odd-row"}
-            >
-              <Car {...car} />
+        <div>
+          <NetworkStatus />
+          {/* Display filtered cars */}
+          <IonGrid className="car-table">
+            <IonRow className="header-row">
+              <IonCol>
+                <strong>ID</strong>
+              </IonCol>
+              <IonCol>
+                <strong>Brand</strong>
+              </IonCol>
+              <IonCol>
+                <strong>Date</strong>
+              </IonCol>
+              <IonCol>
+                <strong>Is new?</strong>
+              </IonCol>
+              <IonCol>
+                <strong>Picture</strong>
+              </IonCol>
+              <IonCol>
+                <strong>Action</strong>
+              </IonCol>
             </IonRow>
-          ))}
-          {filteredCars.map((car, index) => (
-            <IonRow
-              key={car.id}
-              className={index % 2 === 0 ? "even-row" : "odd-row"}
-            >
-              <Car
+            {cars.map((car, index) => (
+              <IonRow
                 key={car.id}
-                id={car.id}
-                brand={car.brand}
-                date={car.date}
-                is_new={car.is_new}
-                car_image={car.car_image}
-              />{" "}
-            </IonRow>
-          ))}
-        </IonGrid>
-        {/* Input field for filtering */}
-        <IonInput
-          placeholder={"Filter by brand"}
-          value={filter}
-          onIonChange={(e) => setFilter(e.detail.value || "")}
-        />
+                className={index % 2 === 0 ? "even-row" : "odd-row"}
+              >
+                <Car {...car} />
+              </IonRow>
+            ))}
+            {filteredCars.map((car, index) => (
+              <IonRow
+                key={car.id}
+                className={index % 2 === 0 ? "even-row" : "odd-row"}
+              >
+                <Car
+                  key={car.id}
+                  id={car.id}
+                  brand={car.brand}
+                  date={car.date}
+                  is_new={car.is_new}
+                  car_image={car.car_image}
+                />{" "}
+              </IonRow>
+            ))}
+          </IonGrid>
+          {/* Input field for filtering */}
+          <IonInput
+            placeholder={"Filter by brand"}
+            value={filter}
+            onIonChange={(e) => setFilter(e.detail.value || "")}
+          />
 
-        <IonButton
-          onClick={(e) => {
-            e.preventDefault();
-            history.push("/carsadd");
-          }}
-        >
-          Add cars
-        </IonButton>
+          <IonButton
+            onClick={(e) => {
+              e.preventDefault();
+              history.push("/carsadd");
+            }}
+          >
+            Add cars
+          </IonButton>
+        </div>
+        <div>
+          <IonButton onClick={handlePreviousPage} disabled={currentPage === 1}>
+            Previous
+          </IonButton>
+          <span>Page: {currentPage}</span>
+          <IonButton onClick={handleNextPage} disabled={currentPage === 100}>
+            Next
+          </IonButton>
+        </div>
       </div>
-      <div>
-        <IonButton onClick={handlePreviousPage} disabled={currentPage === 1}>
-          Previous
-        </IonButton>
-        <span>Page: {currentPage}</span>
-        <IonButton onClick={handleNextPage} disabled={currentPage === 100}>
-          Next
-        </IonButton>
-      </div>
-    </div>
+    </IonContent>
   );
 };
 
